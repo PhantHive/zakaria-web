2710e83cc3543312f04b84e3fe4a6940bbeef4be
PhantHive <phantomhive72@yahoo.fr>
2023-12-17 19:21:14 +0100

    feat: border color change effect

--- src/animation/displayMyWork.ts
+++ src/animation/displayMyWork.ts
@@ -2,10 +2,17 @@ import { ref, onMounted, computed } from 'vue';
 
 const useImageSlider = () => {
     const images = ref<string[]>([
+        'mywork/mastermind.png',
+        'mywork/aircraft-stability.png',
         'mywork/ania-bot.png',
         'mywork/crossword-generator.png',
-        'mywork/kbtool.gif',
+        'mywork/kb-tool-names.png',
+        'mywork/matrix-calculator.png',
+        'mywork/minesweeper-resolver.png',
+        'mywork/opti-resolver.png',
         'mywork/pheabots.png',
+        'mywork/zilya-bot.png',
+        'mywork/bigbrain.png'
     ]);
     let imageIndex = ref(0);
     const currentImage = computed(() => images.value[imageIndex.value]);
